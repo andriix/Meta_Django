a118637bb05e24e2edd0fc9cdc8db0507d5d82b7
Andrii X <khrinenko.andrew@gmail.com>
2026-03-03 16:34:32 +0200

    created menu and booking models

--- littlelemon/apps/restaurant/admin.py
+++ littlelemon/apps/restaurant/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
 # Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "inventory")
+    search_fields = ("title",)
+    list_filter = ("price",)
+    ordering = ("title",)
+    list_editable = ("price", "inventory")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("name", "no_of_guests", "booking_date")
+    search_fields = ("name",)
+    list_filter = ("booking_date",)
+    ordering = ("-booking_date",)
+    date_hierarchy = "booking_date"
