9c2ab4cd8d6ab5b07640318ef29eb4e201e2b4b9
Andrii X <khrinenko.andrew@gmail.com>
2026-03-04 15:09:02 +0200

    booking API

--- littlelemon/apps/restaurant/serializers.py
+++ littlelemon/apps/restaurant/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Menu
+from .models import Menu, Booking
 from django.contrib.auth.models import User
 
 class MenuSerializer(serializers.ModelSerializer):
@@ -13,7 +13,14 @@ class MenuSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("negative price")
         return value
 
+class BookingSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Booking
+          fields = ['id', 'name', 'no_of_guests', 'booking_date']
+          read_only_fields = ['id']
+
 class UserSerializer(serializers.ModelSerializer):
         class Meta:
             model = User
             fields = ['url', 'username', 'email', 'groups']
+
